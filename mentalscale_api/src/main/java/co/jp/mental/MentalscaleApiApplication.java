78f1590cb9ce58a82b804986ca4f07f8853bcefb
unknown <go@revale.co.jp>
2025-10-19 20:29:08 +0900

    新規プロジェクト

---
+++ mentalscale_api/src/main/java/co/jp/mental/MentalscaleApiApplication.java
@@ -0,0 +1,13 @@
+package co.jp.mental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalscaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalscaleApiApplication.class, args);
+	}
+
+}
---
+++ mentalscale_api/src/test/java/co/jp/mental/MentalscaleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jp.mental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalscaleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
