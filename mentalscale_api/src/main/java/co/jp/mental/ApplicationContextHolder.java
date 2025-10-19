149313678ec176aeebf50b64ba43bd67da6ba255
go <go@revale.co.jp>
2025-10-20 01:00:16 +0900

    共通部品／DI関係クラス直接呼び出す部品追加、サービスクラス修正

---
+++ mentalscale_api/src/main/java/co/jp/mental/ApplicationContextHolder.java
@@ -0,0 +1,28 @@
+package co.jp.mental;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextHolder implements ApplicationContextAware{
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext arg) throws BeansException {
+        
+        context= arg;
+        
+    }
+
+
+    public static <T> T getBean(String name, Class<T> type) {
+        return context.getBean(name,type);
+    }
+    
+    public static Object getBean(String name) {
+        return context.getBean(name);
+    }
+}
--- mentalscale_api/src/main/java/co/jp/mental/service/BasicService.java
+++ mentalscale_api/src/main/java/co/jp/mental/service/BasicService.java
@@ -1,5 +1,10 @@
 package co.jp.mental.service;
 
+import co.jp.mental.Input;
+import co.jp.mental.Output;
+
 public interface BasicService {
+
+    public Output run(Input input);
 
 }
