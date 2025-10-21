3dd14b93dcebc1ead0dcc407ecf4f9226f7a5c31
go <go@revale.co.jp>
2025-10-21 21:48:35 +0900

    issue #5: 新規製造（柄だけ）

---
+++ mentalscale_api/src/main/java/co/jp/mental/domain/mi/MentalInquiryController.java
@@ -0,0 +1,16 @@
+package co.jp.mental.domain.mi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.jp.mental.domain.BasicControl;
+
+/**
+ * メンタルスケール結果照会。
+ */
+@RestController
+@RequestMapping("/test")
+public class MentalInquiryController implements BasicControl{
+    
+    
+}
---
+++ mentalscale_api/src/main/java/co/jp/mental/service/mi/MentalInquiryService.java
@@ -0,0 +1,20 @@
+package co.jp.mental.service.mi;
+
+import org.springframework.stereotype.Service;
+
+import co.jp.mental.Input;
+import co.jp.mental.Output;
+import co.jp.mental.service.BasicService;
+
+/**
+ * メンタルスケール結果照会。
+ */
+@Service
+public class MentalInquiryService implements BasicService<Output, Input>{
+
+    @Override
+    public Output run(Input I) {
+        return null;
+    }
+    
+}
