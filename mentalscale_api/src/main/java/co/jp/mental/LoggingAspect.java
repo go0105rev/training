0877872d7cbf5b52c362e2357473e316567311bb
go <go@revale.co.jp>
2025-10-21 21:00:11 +0900

    共通部品／ログ処理

---
+++ mentalscale_api/src/main/java/co/jp/mental/LoggingAspect.java
@@ -0,0 +1,42 @@
+package co.jp.mental;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    Logger logger= LoggerFactory.getLogger(LoggingAspect.class);
+    
+    @Around("execution(* co.jp.mental.service.*.*(..))")
+    public Object logInterceptor(ProceedingJoinPoint joinPoint) {
+        try {
+
+            logger.info("-->サービス処理を開始しました。");
+            
+            Object o= joinPoint.proceed();
+            
+            logger.info("-->サービス処理を終了しました。");
+            
+            return o;
+
+        } catch (Throwable e) {
+
+            if(e instanceof SystemException se) {
+                logger.error("error -->",se);
+            }else {
+                logger.error("unknowerror -->",e);
+            }
+
+            //TODO NGのjsonレスポンス渡したい。
+            return null;
+
+        }
+    }
+
+}
---
+++ mentalscale_api/src/main/java/co/jp/mental/SystemException.java
@@ -0,0 +1,5 @@
+package co.jp.mental;
+
+public class SystemException extends RuntimeException{
+
+}
