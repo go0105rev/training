7d9b42e0ccf0accbf19e6613c203b6932771ccd0
go <go@revale.co.jp>
2025-10-22 23:23:23 +0900

    issue #5 メンタルスケール結果照会のリクエスト群新規作成

--- mentalscale_api/src/main/java/co/jp/mental/domain/mi/MiInput.java
+++ mentalscale_api/src/main/java/co/jp/mental/domain/mi/MiInput.java
@@ -4,4 +4,48 @@ import co.jp.mental.Input;
 
 public class MiInput implements Input{
 
+    /**
+     * 名前。
+     */
+    private String name;
+    
+    /**
+     * 個人ID。
+     */
+    private String id;
+    
+    /**
+     * 年齢。
+     */
+    private int age;
+    
+    /**
+     * 性別。
+     */
+    private int gender;
+
+    /**
+     * 月。
+     */
+    private int month;
+    
+    /**
+     * 日。
+     */
+    private int date;
+    
+    /**
+     * 所属会社ID。
+     */
+    private int corp;
+
+    /**
+     * 気分。
+     */
+    private int emosion;
+    
+    /**
+     * 体調。
+     */
+    
 }
