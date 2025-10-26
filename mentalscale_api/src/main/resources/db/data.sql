bb14f44e52191d77e81a31f0b4baa59e1e165fa7
go <go@revale.co.jp>
2025-10-26 15:12:51 +0900

    issue #9 出力レベル修正、ライブラリーログレベル設定

--- mentalscale_api/src/main/resources/db/data.sql
+++ mentalscale_api/src/main/resources/db/data.sql
@@ -5,12 +5,12 @@ INSERT INTO b_user_inf(user_id, user_name, corp_id, department, grade, gender, b
 INSERT INTO b_user_inf(user_id, user_name, corp_id, department, grade, gender, birth, career_yrs) VALUES
 ('S000000003','小林 エミ', '001', '001',10,'M','20200304',10);
 
-INSERT INTO b_mental_scale(user_id, mscale_num, s_date) VALUES
-('S000000001','MS00000001',PARSEDATETIME('20251026111500000', 'yyyyMMddHHmmssSSS'));
-INSERT INTO b_mental_scale(user_id, mscale_num, s_date) VALUES
-('S000000002','MS00000002',PARSEDATETIME('20251026100549999', 'yyyyMMddHHmmssSSS'));
-INSERT INTO b_mental_scale(user_id, mscale_num, s_date) VALUES
-('S000000001','MS00000003',PARSEDATETIME('20251026120059999', 'yyyyMMddHHmmssSSS'));
+INSERT INTO b_mental_scale(user_id, mscale_num, s_date, avg_score, max_score, min_score) VALUES
+('S000000001','MS00000001',PARSEDATETIME('20251026111500000', 'yyyyMMddHHmmssSSS'),3.1,5,1);
+INSERT INTO b_mental_scale(user_id, mscale_num, s_date, avg_score, max_score, min_score) VALUES
+('S000000002','MS00000002',PARSEDATETIME('20251026100549999', 'yyyyMMddHHmmssSSS'),2.8,5,1);
+INSERT INTO b_mental_scale(user_id, mscale_num, s_date, avg_score, max_score, min_score) VALUES
+('S000000001','MS00000003',PARSEDATETIME('20251026120059999', 'yyyyMMddHHmmssSSS'),2.2,5,1);
 INSERT INTO b_mental_scale_detail(mscale_num, s_date, scale1, scale2, scale3, scale4, scale5, scale6, scale7, scale8, scale9, scale10, scale11, scale12, scale13, scale14, scale15, scale16, scale17, scale18, scale19, scale20, other) VALUES
 ('MS00000001',PARSEDATETIME('20251026111500000', 'yyyyMMddHHmmssSSS'),'2','2','3','4','2','2','3','4','5','1','5','5','5','5','5','5','1','1','1','4','');
 INSERT INTO b_mental_scale_detail(mscale_num, s_date, scale1, scale2, scale3, scale4, scale5, scale6, scale7, scale8, scale9, scale10, scale11, scale12, scale13, scale14, scale15, scale16, scale17, scale18, scale19, scale20, other) VALUES
