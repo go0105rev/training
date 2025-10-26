3b47825eb39cd40d2ba1e636eb66e8caeabd0a03
go <go@revale.co.jp>
2025-10-26 11:46:33 +0900

    issue #11 テーブル新規作成、仮データ作成、起動時コンソール画面アクセス有効化

--- mentalscale_api/src/main/resources/db/data.sql
+++ mentalscale_api/src/main/resources/db/data.sql
@@ -1,1 +1,19 @@
-INSERT INTO employee(id, name, age) VALUES('1', 'Kato', 10)
+INSERT INTO b_user_inf(user_id, user_name, corp_id, department, grade, gender, birth, career_yrs) VALUES
+('S000000001','田中 太郎', '001', '001',10,'M','20200304',10);
+INSERT INTO b_user_inf(user_id, user_name, corp_id, department, grade, gender, birth, career_yrs) VALUES
+('S000000002','進藤 純一郎', '001', '001',10,'M','20200304',10);
+INSERT INTO b_user_inf(user_id, user_name, corp_id, department, grade, gender, birth, career_yrs) VALUES
+('S000000003','小林 エミ', '001', '001',10,'M','20200304',10);
+
+INSERT INTO b_mental_scale(user_id, mscale_num, s_date) VALUES
+('S000000001','MS00000001',PARSEDATETIME('20251026111500000', 'yyyyMMddHHmmssSSS'));
+INSERT INTO b_mental_scale(user_id, mscale_num, s_date) VALUES
+('S000000002','MS00000002',PARSEDATETIME('20251026100549999', 'yyyyMMddHHmmssSSS'));
+INSERT INTO b_mental_scale(user_id, mscale_num, s_date) VALUES
+('S000000001','MS00000003',PARSEDATETIME('20251026120059999', 'yyyyMMddHHmmssSSS'));
+INSERT INTO b_mental_scale_detail(mscale_num, s_date, scale1, scale2, scale3, scale4, scale5, scale6, scale7, scale8, scale9, scale10, scale11, scale12, scale13, scale14, scale15, scale16, scale17, scale18, scale19, scale20, other) VALUES
+('MS00000001',PARSEDATETIME('20251026111500000', 'yyyyMMddHHmmssSSS'),'2','2','3','4','2','2','3','4','5','1','5','5','5','5','5','5','1','1','1','4','');
+INSERT INTO b_mental_scale_detail(mscale_num, s_date, scale1, scale2, scale3, scale4, scale5, scale6, scale7, scale8, scale9, scale10, scale11, scale12, scale13, scale14, scale15, scale16, scale17, scale18, scale19, scale20, other) VALUES
+('MS00000002',PARSEDATETIME('20251026100549999', 'yyyyMMddHHmmssSSS'),'5','2','3','4','1','2','3','4','5','1','2','3','5','5','5','2','1','1','1','4',null);
+INSERT INTO b_mental_scale_detail(mscale_num, s_date, scale1, scale2, scale3, scale4, scale5, scale6, scale7, scale8, scale9, scale10, scale11, scale12, scale13, scale14, scale15, scale16, scale17, scale18, scale19, scale20, other) VALUES
+('MS00000003',PARSEDATETIME('20251026120059999', 'yyyyMMddHHmmssSSS'),'5','2','3','4','1','2','3','4','5','1','1','1','1','1','1','1','2','2','2','2','最近疲れた');
--- mentalscale_api/src/main/resources/db/schema.sql
+++ mentalscale_api/src/main/resources/db/schema.sql
@@ -1,5 +1,65 @@
-CREATE TABLE IF NOT EXISTS employee(
-    id VARCHAR(50) PRIMARY KEY,
-    name VARCHAR(50),
-    age INT
-    );
+CREATE TABLE IF NOT EXISTS b_user_inf (
+ user_id CHAR(10) NOT NULL PRIMARY KEY,
+ user_name CHAR(20) NOT NULL,
+ corp_id CHAR(3),
+ department CHAR(3),
+ grade INT,
+ gender CHAR(1) NOT NULL,
+ birth TIMESTAMP NOT NULL,
+ career_yrs INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS b_mental_scale (
+ user_id CHAR(10) NOT NULL,
+ mscale_num CHAR(10) NOT NULL PRIMARY KEY,
+ s_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS b_mental_scale_detail (
+ mscale_num CHAR(10) NOT NULL PRIMARY KEY,
+ s_date TIMESTAMP NOT NULL,
+ scale1 INT,
+ scale2 INT,
+ scale3 INT,
+ scale4 INT,
+ scale5 INT,
+ scale6 INT,
+ scale7 INT,
+ scale8 INT,
+ scale9 INT,
+ scale10 INT,
+ scale11 INT,
+ scale12 INT,
+ scale13 INT,
+ scale14 INT,
+ scale15 INT,
+ scale16 INT,
+ scale17 INT,
+ scale18 INT,
+ scale19 INT,
+ scale20 INT,
+ other TEXT
+);
+
+CREATE TABLE IF NOT EXISTS m_mental (
+ ques_scale1 TEXT NOT NULL,
+ ques_scale2 TEXT NOT NULL,
+ ques_scale3 TEXT NOT NULL,
+ ques_scale4 TEXT NOT NULL,
+ ques_scale5 TEXT NOT NULL,
+ ques_scale6 TEXT NOT NULL,
+ ques_scale7 TEXT NOT NULL,
+ ques_scale8 TEXT NOT NULL,
+ ques_scale9 TEXT NOT NULL,
+ ques_scale10 TEXT NOT NULL,
+ ques_scale11 TEXT NOT NULL,
+ ques_scale12 TEXT NOT NULL,
+ ques_scale13 TEXT NOT NULL,
+ ques_scale14 TEXT NOT NULL,
+ ques_scale15 TEXT NOT NULL,
+ ques_scale16 TEXT NOT NULL,
+ ques_scale17 TEXT NOT NULL,
+ ques_scale18 TEXT NOT NULL,
+ ques_scale19 TEXT NOT NULL,
+ ques_scale20 TEXT NOT NULL
+);
