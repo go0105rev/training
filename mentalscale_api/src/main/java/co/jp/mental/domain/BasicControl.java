7b8d2339eb358f877e0b85cb67c8427980c45dc6
go <go@revale.co.jp>
2025-10-26 15:17:55 +0900

    issue #10 コントローラーの共通処理修正

--- mentalscale_api/src/main/java/co/jp/mental/domain/BasicControl.java
+++ mentalscale_api/src/main/java/co/jp/mental/domain/BasicControl.java
@@ -1,7 +1,11 @@
 package co.jp.mental.domain;
 
+import jakarta.validation.constraints.NotNull;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import co.jp.mental.ApplicationContextHolder;
@@ -13,18 +17,20 @@ public abstract class BasicControl<O, I> {
 
     Logger logger = LoggerFactory.getLogger(BasicControl.class);
 
-    public Output controller(String action, Input input) {
+    public Output controller(String action, @NotNull Input input) {
 
+        logger.info(action + "コントローラー処理を開始しました。-->" + input.toString());
         BasicService<Output, Input> bs = ApplicationContextHolder.getBean(action + "Service", BasicService.class);
-        logger.info(action + "コントローラー処理を開始しました。-->" + bs.toString());
         Output result=bs.run(input);
-        logger.info(action + "コントローラー処理を終了しました。-->" + bs.toString());
+        logger.info(action + "コントローラー処理を終了しました。-->" + input.toString());
 
         return result;
 
     }
 
+    @GetMapping
     public abstract O doGet();
 
+    @PostMapping
     public abstract O doPost(@RequestBody I input);
 }
