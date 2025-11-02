44a2858dd8647ad0f66ff801892832e04a2bfcb1
go <go@revale.co.jp>
2025-11-02 14:03:35 +0900

    issue #5 メンタルスケール照会の詳細出力できるようにfix

--- mentalscale_api/src/main/java/co/jp/mental/service/mi/MentalInfo.java
+++ mentalscale_api/src/main/java/co/jp/mental/service/mi/MentalInfo.java
@@ -4,23 +4,36 @@ import java.util.List;
 
 import co.jp.mental.BusinessInfo;
 import co.jp.mental.repository.MentalScale;
+import co.jp.mental.repository.MentalScaleDetail;
 
 
 public class MentalInfo implements BusinessInfo{
 
-    private List<MentalScale> me;
+    private List<MentalScale> ms;
+    
+    private List<MentalScaleDetail> msd;
 
-    public List<MentalScale> getMe() {
-        return me;
+    public List<MentalScale> getMs() {
+        return ms;
     }
 
-    public void setMe(List<MentalScale> me) {
-        this.me = me;
+    public void setMs(List<MentalScale> ms) {
+        this.ms = ms;
+    }
+    
+    
+
+    public List<MentalScaleDetail> getMsd() {
+        return msd;
+    }
+
+    public void setMsd(List<MentalScaleDetail> msd) {
+        this.msd = msd;
     }
 
     @Override
     public String toString() {
-        return "MentalInfo [me=" + me + "]";
+        return "MentalInfo [ms=" + ms + ", msd=" + msd + "]";
     }
     
     
