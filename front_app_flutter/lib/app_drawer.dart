ec2fd7cef44bb0b2c3ddca142f2478dcffc9d637
go0105rev <go@revale.co.jp>
2025-11-23 04:00:39 +0900

    画面単位でファイル分割

--- front_app_flutter/lib/app_drawer.dart
+++ front_app_flutter/lib/app_drawer.dart
@@ -1,150 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'menu_scale_input.dart';
+import 'menu_scale_history.dart';
+import 'menu_scale_detail.dart';
 import 'package:front_app_flutter/l10n/app_localizations.dart';
-import 'dart:async';
 
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      locale: Locale('zh', 'TW'),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('ja'),
-        Locale('zh', 'TW'),
-      ],
-      title: 'Myメンタルスケール管理',
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    // 2秒後にMainScreenに遷移
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const MainScreen()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(
-              Icons.psychology,
-              size: 100,
-              color: Colors.blue,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'MyMs Manager',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MenuScaleInput extends StatefulWidget {
-  @override
-  _MenuScaleInputState createState() => _MenuScaleInputState();
-}
-
-class _MenuScaleInputState extends State<MenuScaleInput> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Scale Input'),
-      ),
-      body: Center(
-        child: Text('Scale Input Screen'),
-      ),
-    );
-  }
-}
-
-class MenuScaleHistory extends StatefulWidget {
-  @override
-  _MenuScaleHistoryState createState() => _MenuScaleHistoryState();
-}
-
-class _MenuScaleHistoryState extends State<MenuScaleHistory> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Scale History'),
-      ),
-      body: Center(
-        child: Text('Scale History Screen'),
-      ),
-    );
-  }
-}
-
-class MenuScaleDetail extends StatefulWidget {
-  @override
-  _MenuScaleDetailState createState() => _MenuScaleDetailState();
-}
-
-class _MenuScaleDetailState extends State<MenuScaleDetail> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Scale Detail History'),
-      ),
-      body: Center(
-        child: Text('Scale Detail History Screen'),
-      ),
-    );
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar.new(
-          title: const Text('Myメンタルスケール管理'),
-        ),
-
-        drawer: Drawer(
+    return Drawer(
           child: Builder(builder: (context) { return 
             ListView(
               padding: EdgeInsets.zero,
@@ -170,7 +35,7 @@ class MainScreen extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MenuScaleInput(), // 遷移先 Screen
+                        builder: (_) => MenuScaleInput(), // 遷移先 Screen
                       ),
                     );
                   },
@@ -184,7 +49,7 @@ class MainScreen extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MenuScaleHistory(), // 遷移先 Screen
+                        builder: (_) => MenuScaleHistory(), // 遷移先 Screen
                       ),
                     );
                   },
@@ -198,7 +63,7 @@ class MainScreen extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MenuScaleDetail(), // 遷移先 Screen
+                        builder: (_) => MenuScaleDetail(), // 遷移先 Screen
                       ),
                     );
                   },
@@ -206,11 +71,6 @@ class MainScreen extends StatelessWidget {
               ],
             );
           }),
-        ),
-
-        body: Center(
-          child: Text('MyMs Manager'),
-        ),
-    );
+        );
   }
 }
