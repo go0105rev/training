7d3aeb568260e7e4da52b09cb3aa62fcbb944017
go0105rev <go@revale.co.jp>
2025-11-22 19:16:47 +0900

    drawer部分新規作成

--- front_app_flutter/lib/main.dart
+++ front_app_flutter/lib/main.dart
@@ -9,8 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+
+        appBar: AppBar.new(
+          title: const Text('Myメンタルスケール管理'),
+        ),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'MyMs管理 menu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.scale),
+                title: Text('スケール入力'),
+              ),
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text('スケール履歴'),
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text('スケール詳細履歴'),
+              ),
+            ],
+          ),
+        ),
+
         body: Center(
           child: Text('Hello World!'),
         ),
